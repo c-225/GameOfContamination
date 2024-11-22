e428472cdf21d9ad7084e423ab1806aba10a5c9d
TriForMine <quentin@triformine.dev>
2024-11-22 16:17:23 +0100

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import {defineConfig} from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: process.env.GITHUB_ACTIONS ? "/Delixia/" : "/",
+	base: process.env.GITHUB_ACTIONS ? "/game-of-life/" : "/",
 })
