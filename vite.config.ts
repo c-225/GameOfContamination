89d833dac333b8759cb0e2d85ebc9651a560844f
TriForMine <quentin@triformine.dev>
2024-12-18 08:32:15 +0100

    make the modal more responsive

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import {defineConfig} from 'vite'
+import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: process.env.GITHUB_ACTIONS ? "/game-of-life/" : "/",
+	css: {
+		postcss: {
+			plugins: [tailwindcss()]
+		}
+	}
 })
